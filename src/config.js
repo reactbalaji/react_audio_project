358cc3fa6933f9b610dbdc8d46485f34f3e9c362
Balaji Anandhan <a-balaji.a@aaratech.ad>
2024-09-30 18:55:41 +0530

    code changes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-//export const audioUpload = 'http://13.232.8.207:5555/audio/upload'; //Aws 
-//export const cognitoSignin = 'http://13.232.8.207:5555/auth/signin';
-export const audioUploadUrl = 'http://localhost:5555/audio/upload'; //Aws 
-export const cognitoSignin = 'http://localhost:5555/auth/signin';
+export const audioUploadUrl = 'http://13.232.8.207:5555/audio/upload'; //Aws 
+export const cognitoSignin = 'http://13.232.8.207:5555/auth/signin';
+//export const audioUploadUrl = 'http://localhost:5555/audio/upload'; //Aws 
+//export const cognitoSignin = 'http://localhost:5555/auth/signin';
